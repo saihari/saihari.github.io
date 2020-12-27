de5a2b77d8cedab2cb2926464fa6e55839bc8658
Sai Hari <saihari.m@gmail.com>
2020-12-27 23:38:31 +0530

    Updates

---
+++ precache-manifest.3a7173b91c950b82241eee659c0a0779.js
@@ -0,0 +1,122 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "263950cc52fe43b2bd13861065e90658",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2f022a9197fbe8b4d137",
+    "url": "/static/css/2.dc5ff711.chunk.css"
+  },
+  {
+    "revision": "4cd831c33f52c8005e31",
+    "url": "/static/css/main.2234da1c.chunk.css"
+  },
+  {
+    "revision": "2f022a9197fbe8b4d137",
+    "url": "/static/js/2.082150d5.chunk.js"
+  },
+  {
+    "revision": "4cd831c33f52c8005e31",
+    "url": "/static/js/main.7676b62f.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "27e8e879248e8f2da86baffc53965715",
+    "url": "/static/media/Aws.27e8e879.png"
+  },
+  {
+    "revision": "4c5ed89490f07b7be7f4e6d496060f5f",
+    "url": "/static/media/Docker.4c5ed894.png"
+  },
+  {
+    "revision": "032507eda8389a30b18de4d092a376fa",
+    "url": "/static/media/FastAi.032507ed.png"
+  },
+  {
+    "revision": "acbadef090916fe5c010748a62ac99b5",
+    "url": "/static/media/Gear.acbadef0.png"
+  },
+  {
+    "revision": "5a84d5d7850aeed12df857d09493b6f9",
+    "url": "/static/media/JavaScript.5a84d5d7.png"
+  },
+  {
+    "revision": "6ed7c2eb6b0a9aa6ddbd0b1956dfd954",
+    "url": "/static/media/MSSqlServer.6ed7c2eb.png"
+  },
+  {
+    "revision": "37bce99c842423736ee35a7845be8e6e",
+    "url": "/static/media/Matplotlib.37bce99c.png"
+  },
+  {
+    "revision": "890d679801e3af20235e12b19357ed59",
+    "url": "/static/media/MongoDB.890d6798.png"
+  },
+  {
+    "revision": "ad9dfa1dee58e6bdf360c9f1c59cfe8b",
+    "url": "/static/media/Numpy.ad9dfa1d.png"
+  },
+  {
+    "revision": "c6aa5733771752e1006b1119d5bcf4c3",
+    "url": "/static/media/Pipeline.c6aa5733.PNG"
+  },
+  {
+    "revision": "76fc9aa252b70c510358bf5b9b4c1abc",
+    "url": "/static/media/Python.76fc9aa2.png"
+  },
+  {
+    "revision": "f006806312dbf953a01945bc57c25740",
+    "url": "/static/media/Pytorch.f0068063.png"
+  },
+  {
+    "revision": "580ffd26ccaa16481ed87c1829c74943",
+    "url": "/static/media/React.580ffd26.png"
+  },
+  {
+    "revision": "4f8a7fb3b684a6b6e6cecca21c3c6977",
+    "url": "/static/media/Rlogo.4f8a7fb3.png"
+  },
+  {
+    "revision": "3954db7b8a8728d26bb1dbc9604d3802",
+    "url": "/static/media/SAS.3954db7b.jpg"
+  },
+  {
+    "revision": "73ea098f98fc962e4bbdd40ed1161c1d",
+    "url": "/static/media/SQL.73ea098f.jfif"
+  },
+  {
+    "revision": "964a9cebc6ba6fbfd32c02ece5f988e8",
+    "url": "/static/media/ScikitLearn.964a9ceb.png"
+  },
+  {
+    "revision": "a3d75feedc62105ad701ed29741c0268",
+    "url": "/static/media/Selenium.a3d75fee.png"
+  },
+  {
+    "revision": "7a473faea349e24c4e441413d60ac0dd",
+    "url": "/static/media/Tensorflow.7a473fae.jpg"
+  },
+  {
+    "revision": "4a47eff6856e4d14e157f796e7851cc5",
+    "url": "/static/media/icon.4a47eff6.png"
+  },
+  {
+    "revision": "0b4ac1dc75df35e169b70d7719afe4cc",
+    "url": "/static/media/notification.0b4ac1dc.ttf"
+  },
+  {
+    "revision": "5bee74caefdf9d0a834915f6c8eeb259",
+    "url": "/static/media/notification.5bee74ca.svg"
+  },
+  {
+    "revision": "651771e1df95c807c99608188d0a4287",
+    "url": "/static/media/notification.651771e1.woff"
+  },
+  {
+    "revision": "c0d3c94cd6112550c51d7d1ed13b9da1",
+    "url": "/static/media/notification.c0d3c94c.eot"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/saihari.github.io/precache-manifest.128c6729ed304cf496c4e041391353f2.js"
+  "/precache-manifest.3a7173b91c950b82241eee659c0a0779.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/saihari.github.io/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
